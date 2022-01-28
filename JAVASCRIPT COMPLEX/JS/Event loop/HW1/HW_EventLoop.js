0724cb3338f8f752e2c2ad45475c9f601181bab3
DmitryKhirash <91758248+DmitryKhirash@users.noreply.github.com>
2022-01-28 17:51:01 +0200

    inProcessing

--- JAVASCRIPT COMPLEX/JS/Event loop/HW1/HW_EventLoop.js
+++ JAVASCRIPT COMPLEX/JS/Event loop/HW1/HW_EventLoop.js
@@ -9,172 +9,306 @@
 // Піти в душ - 0.5с // Дочекатись автобус - 3с //
 // Пообідати - 1с // // І так далі
 
+
+//Method Callback//
+// let mood = 10;
+// let money = 0;
+//
+// function toWakeUp(isDone, callback) {
+//     setTimeout(() => {
+//         if (isDone) {
+//             console.log(mood, 'It`s fanny day for me. Go to brash teeth')
+//             mood += 20;
+//             callback(null, mood)
+//         } else {
+//             callback('Ops I overslept', null)
+//         }
+//     }, 300)
+// }
+//
+// function brashTeeth(myMood, callback) {
+//     setTimeout(() => {
+//         if (myMood > 10) {
+//             console.log(mood, 'Norm=). Very well. Clean. To cook breakfast)')
+//             mood += 15
+//             callback(null, mood)
+//         } else {
+//             callback('Try again', null)
+//         }
+//     }, 500)
+// }
+//
+// function goToShower(fresh, callback) {
+//     setTimeout(() => {
+//         if (fresh >= 45) {
+//             console.log(mood, 'Hmmm, goodness. Go to shower.');
+//             mood += 20;
+//             callback(null, mood)
+//         } else {
+//             callback('Ops. I lost my breakfast', null)
+//         }
+//     }, 400)
+// }
+//
+// function toGetDressed(showClothes, callback) {
+//     setTimeout(() => {
+//         if (showClothes >= 52) {
+//             console.log(mood, 'It`s time to pick up clothes')
+//             mood += 10
+//             callback(null, 'You look grate. Run to work!')
+//         } else {
+//             console.log('Ops', null)
+//         }
+//     }, 200)
+// }
+//
+// function busStop(waitListenMusic, callback) {
+//         setTimeout(()=>{
+//             if (waitListenMusic>=60){
+//                 console.log(mood, 'Time go to bus stop')
+//                 mood += 5;
+//                 callback(null, 'Oh my bus');
+//             } else {
+//                 callback('Dou', null)
+//             }
+//         },450)
+// }
+//
+// function workTime(work, jobDone, callback) {
+//         setTimeout(()=>{
+//             if (work>=70){
+//                 mood +=30
+//                 console.log(mood,'I like listen to music. And now I ready to progressing for work')
+//                 money += 500;
+//                 console.log(mood,'Cool working today. How much did I earn today?')
+//                 console.log(money,'$ Wow,it`s good! Go to meet friend')
+//                 callback(null, 'goMeet')
+//             } else {
+//                 callback('Dou', null)
+//             }
+//         },1000)
+// }
+//
+// function meetFriends(toPay, field, callback) {
+//         setTimeout(()=>{
+//             if (toPay >= 10){
+//                 money -=10
+//                 mood +=20
+//                 console.log(mood, 'Go to the ice rink. It`s amazing!');
+//                 console.log(money, '$ Ok')
+//                 callback(null, 'Fine meet')
+//             } else {
+//                 console.log('Dou', null)
+//             }
+//         },600)
+// }
+//
+// function relax(relax, callback) {
+//         setTimeout(()=>{
+//             if (relax >=110){
+//                 mood += 15;
+//                 console.log(mood, 'Time for reed a book')
+//                 callback(null, 'Grate time')
+//             } else {
+//                 console.log('Dou', null);
+//             }
+//         },559)
+// }
+//
+// function dream(dream, callback) {
+//         setTimeout(()=>{
+//             if (dream){
+//                 mood += 40;
+//                 console.log(mood, 'Forward to the future! Zzzzz...')
+//                 callback(null, 'Zzzz')
+//             } else {
+//                 console.log('Dou')
+//             }
+//         },1000)
+// }
+//
+//
+// toWakeUp(mood, () => {
+//     brashTeeth(mood, () => {
+//         goToShower(mood, () => {
+//             toGetDressed(mood, () => {
+//                 busStop(mood, () => {
+//                     workTime(mood, money, () => {
+//                         meetFriends(money, mood, () => {
+//                             relax(mood, () => {
+//                                 dream(mood, () => {
+//
+//                                 });
+//
+//                             });
+//
+//                         });
+//
+//                     });
+//
+//                 });
+//
+//             });
+//
+//         });
+//
+//     });
+// });
+
+
+let mood = 10;
 let money = 0;
 
 function toWakeUp(isDone) {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
             if (isDone) {
-                resolve('I`m grate')
+                console.log(mood, 'It`s fanny day for me.')
+                mood += 20;
+                resolve(mood)
             } else {
                 reject('Ops I overslept')
             }
         }, 300)
-    })
-
+    });
 }
 
-function brashTeeth(clean) {
+function brashTeeth(myMood) {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
-            if (clean) {
-                resolve('Clean. To cook breakfast')
+            if (myMood > 10) {
+                mood += 15
+                console.log(mood, 'I`m clean.')
+                resolve(mood)
             } else {
                 reject('Try again')
             }
         }, 500)
     });
-
 }
 
-function breakfast(satisfaid) {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            if (satisfaid) {
-                resolve('I`m full. Go to shower')
-            } else {
-                reject('Ops')
-            }
-        }, 1000)
-    });
-
-}
 
 function goToShower(fresh) {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
-            if (fresh) {
-                resolve('I`m clean and ready to get dresses.')
+            if (fresh >= 45) {
+                mood += 20;
+                resolve(mood)
             } else {
-                reject('Ops')
+                reject('Ops. I lost my breakfast');
             }
         }, 400)
     });
+}
 
+function toBreakfast(cook) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (cook >= 60) {
+                mood += 5;
+                resolve(mood);
+            } else {
+                reject('Dou')
+            }
+        }, 450)
+    });
 }
 
 function toGetDressed(showClothes) {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
-            if (showClothes) {
-                resolve('You look grate. Run to work!')
+            if (showClothes >= 52) {
+                mood += 10
+                resolve(mood)
             } else {
                 reject('Ops')
             }
         }, 200)
     });
-
 }
 
-function busStop(wait) {
+function wayToWork(waitListenMusic) {
     return new Promise((resolve, reject) => {
-        setTimeout(()=>{
-            if (wait){
-                resolve('Oh my bus');
+        setTimeout(() => {
+            if (waitListenMusic >= 60) {
+                mood += 5;
+                resolve(mood);
             } else {
                 reject('Dou')
             }
-        },450)
+        }, 450)
     });
 }
 
-function workTime(jobDone) {
+function workTime(work, jobDone) {
     return new Promise((resolve, reject) => {
-        setTimeout(()=>{
-            if (jobDone){
-                console.log('How much did I earn today');
+        setTimeout(() => {
+            if (work >= 70) {
+                mood += 30
                 money += 500;
-                resolve(money)
+                resolve(mood)
             } else {
-                reject('Dou')
+                reject('Dou');
             }
-        },1000)
+        }, 1000)
     });
 }
 
-function meetFriends(iceSkating) {
-    return new Promise((resolve, reject) => {
-        setTimeout(()=>{
-            if (iceSkating > 10){
-                resolve('Go to the ice rink')
-            } else {
-                reject('Dou')
-            }
-        },600)
-    });
+function meetFriends(toPay, field) {
+    setTimeout(()=>{
+        if (toPay >= 10){
+            money -=10
+            mood +=20
+            console.log(mood, 'Go to the ice rink. It`s amazing!');
+            console.log(money, '$ Ok')
+            callback(null, 'Fine meet')
+        } else {
+            console.log('Dou', null)
+        }
+    },600)
 }
 
 function relax(relax) {
-    return new Promise((resolve, reject) => {
-        setTimeout(()=>{
-            if (relax){
-                resolve('Grate time')
-            } else {
-                reject('Dou')
-            }
-        },559)
-    });
+    setTimeout(()=>{
+        if (relax >=110){
+            mood += 15;
+            console.log(mood, 'Time for reed a book')
+            callback(null, 'Grate time')
+        } else {
+            console.log('Dou', null);
+        }
+    },559)
 }
 
 function dream(dream) {
-    return new Promise((resolve, reject) => {
-        setTimeout(()=>{
-            if (dream){
-                resolve('Zzzzzz')
-            } else {
-                reject('Dou')
-            }
-        },1000)
-    });
+    setTimeout(()=>{
+        if (dream){
+            mood += 40;
+            console.log(mood, 'Forward to the future! Zzzzz...')
+            callback(null, 'Zzzz')
+        } else {
+            console.log('Dou')
+        }
+    },1000)
 }
-toWakeUp(true)
-    .then(result => {
-        console.log(result)
-        return brashTeeth(result)
-    })
-    .then(result => {
-        console.log(result)
-        return breakfast(result)
-    })
-    .then(result => {
-        console.log(result)
-        return goToShower(result)
-    })
-    .then(result => {
-        console.log(result)
-        return toGetDressed(result)
-    })
-    .then(result => {
-        console.log(result)
-        return busStop(result)
-    })
-    .then(salary => {
-        console.log(salary)
-        return workTime(salary)
-    })
-    .then(result =>{
-        console.log(result, '$')
-        return meetFriends(result);
-    })
-    .then(result =>{
-        console.log(result)
-        return relax(result)
-    })
-    .then(result =>{
-        console.log(result);
-        return dream(result);
-    })
-    .then(result =>{
-        console.log(result);
-    })
 
+async function weekDay() {
+    const getUp = await toWakeUp(true);
+        console.log(getUp, 'Go to brash teeth.');
+    const brashT = await brashTeeth(getUp);
+        console.log(brashT, 'Go to shower.');
+    const shower = await goToShower(brashT);
+        console.log(shower, 'Fine. Time for breakfast');
+    const breakfast = await toBreakfast(shower);
+        console.log(breakfast, 'Mmmm, ty-ty-dy...')
+    const clothing = await toGetDressed(breakfast);
+        console.log(clothing, 'It`s time to pick up clothes. You look grate. Run to work!')
+    const wayInRoad = await wayToWork(clothing);
+        console.log(wayInRoad,'I like listen to music. And now I ready to progressing for work' )
+    const workingMood = await workTime(wayInRoad)
+        console.log(workingMood,  'Cool working today. How much did I earn today?')
+    const salary = await workTime(workingMood)
+        console.log(money, '$ Wow,it`s good! Go to meet friend')
+}
+
+weekDay()
