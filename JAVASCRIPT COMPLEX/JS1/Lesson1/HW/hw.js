25ce0e192de779602c321c6a1f93b92cf821f4dc
DmitryKhirash <91758248+DmitryKhirash@users.noreply.github.com>
2022-03-08 12:32:30 +0200

    edd

---
+++ JAVASCRIPT COMPLEX/JS1/Lesson1/HW/hw.js
@@ -0,0 +1,198 @@
+
+
+
+// - Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+//     Вивести кожну змінну за допомогою: console.log , alert, document.write
+// let a = 'hello';
+// console.log(a);
+// alert(a);
+// document.write(a);
+//
+// let b = 'owu';
+// console.log(b);
+// alert(b);
+// document.write(b);
+//
+// let c = 'com';
+// console.log(c);
+// alert(c);
+// document.write(c);
+//
+// let d = 'ua';
+// console.log(d);
+// alert(d);
+// document.write(d);
+//
+// let n1 = 1;
+// console.log(n1);
+// alert(n1);
+// document.write(n1);
+//
+// let n2 = 10;
+// console.log(n2);
+// alert(n2);
+// document.write(n2);
+//
+// let n3 = -999;
+// console.log(n3);
+// alert(n3);
+// document.write(n3);
+//
+// let n4 = 123;
+// console.log(n4);
+// alert(n4);
+// document.write(n4);
+//
+// let n5 = 3.14;
+// console.log(n5);
+// alert(n5);
+// document.write(n5);
+//
+// let n6 = 2.7;
+// console.log(n6);
+// alert(n6);
+// document.write(n6);
+//
+// let n7 = 16;
+// console.log(n7);
+// alert(n7);
+// document.write(n7);
+//
+// let bool1 = false;
+// console.log(bool1);
+// alert(bool1);
+// document.write(bool1);
+//
+// let bool2 = true;
+// console.log(bool2);
+// alert(bool2);
+// document.write(bool2);
+
+// - Переприсвоїти кожній змінній з завдання значення на довільне.
+//     Вивести кожну змінну за допомогою: console.log , alert, document.write
+// let a = 10 > 'hello';
+// console.log(a);
+// alert(a);
+// document.write(a);
+//
+// let b = "owu";
+// console.log(b);
+// alert(b);
+// document.write(b);
+//
+// let c = ` `;
+// console.log(c);
+// alert(c);
+// document.write(c);
+//
+// let d = 'ua' + 100;
+// console.log(d);
+// alert(d);
+// document.write(d);
+//
+// let n1 = 'abc'- 1;
+// console.log(n1);
+// alert(n1);
+// document.write(n1);
+//
+// let n2 = '10';
+// console.log(n2);
+// alert(n2);
+// document.write(n2);
+//
+// let n3 = undefined;
+// console.log(n3);
+// alert(n3);
+// document.write(n3);
+//
+// let n4 = `123`;
+// console.log(n4);
+// alert(n4);
+// document.write(n4);
+//
+// let n5 = ((3.14 + 2) - 'false');
+// console.log(n5);
+// alert(n5);
+// document.write(n5);
+
+// let n6 = !!2.7;
+// console.log(+n6);
+// alert(n6);
+// document.write(n6);
+//
+// let n7 = 16 + 'stars';
+// console.log(n7);
+// alert(n7);
+// document.write(n7);
+//
+// let bool1 = "false";
+// console.log(bool1);
+// alert(bool1);
+// document.write(bool1);
+//
+// let bool2 = true;
+// console.log(typeof bool2);
+// alert(bool2);
+// document.write(bool2);
+
+// - Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person. (Не об'єкт)
+// - За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками.
+//     Вивести "Вітаю *Імя* *По батькові*. Тобі *вік* років".
+
+// let firstName = 'Dmytro ';
+// let middleName = 'Vitaliyovych '
+// let lastName = 'Khirash';
+// let person = firstName + middleName + lastName
+// console.log(person)
+
+// - За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+//     let a = 100; let b = '100'; let c = true;
+
+// let a = 100;
+// console.log(typeof a);
+// let b = '100';
+// console.log(typeof b);
+// let c = true;
+// console.log(typeof c);
+
+// - Визначити відповідний оператор в виразах що б вийшов відповідний результат.
+//     В однакових виразах не використовувати однакові оператори!!!
+//     5 ? 6 -> true
+// 5 ? 6 -> false
+// 5 ? 6 -> false
+// 5 ? 6 -> false
+// 10 ? 10 -> true
+// 10 ? 10 -> true
+// 10 ? 10 -> false
+// 10 ? 10 -> false
+// 10 ? 10 -> false
+// 123 ? '123' -> false
+// 123 ? '123' -> true
+//
+
+let a = 5 > 6;
+console.log(a);
+let b = 5 >= 6;
+console.log(b);
+let c = 5 === 6;
+console.log(c);
+let d = 10 <= 10;
+console.log(d);
+let e = 10 === 10;
+console.log(e);
+let f = 10 < 10;
+console.log(f);
+let g = 10 > 10;
+console.log(g);
+let h = 10 !== 10;
+console.log(h);
+let i = 123 > '123';
+console.log(i);
+let j = 123 !== '123';
+console.log(j)
+
+
+// - Подумайте Какие значения выведет в окно браузера следующий фрагмент кода?  и почему?
+//     let str = "20";
+// let a = 5;
+// documen
